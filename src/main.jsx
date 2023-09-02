00629f5fa1885a54c4f52bd722b60ffd0b9fe31e
TEJAS <tejaskh3@gmail.com>
2023-09-02 15:04:22 +0530

    Merge 'directory' branch into 'main'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <App></App>
+    </Router>
+  </React.StrictMode>
+);
